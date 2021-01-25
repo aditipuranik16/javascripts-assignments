30b0f04df7eb88b04effedc783ab2abbf1e888da
aditipuranik16 <61420553+aditipuranik16@users.noreply.github.com>
2021-01-25 21:43:05 +0530

    update assignment2.js

--- assignment2/assignment2.js
+++ assignment2/assignment2.js
@@ -6,10 +6,8 @@ console.log(clothes[0]);
 //output -: undefined
 
 /*length property of the array object has a special behavior:
-Reducing the value of the length property has the side-effect of deleting own array 
-elements whose array index is between the old and new length values.Because of this
-length behavior, when JavaScript executes clothes.length = 0, 
-all the items of the array clothes are deleted.
+Reducing the value of the length property has the side-effect of deleting own array elements whose array index is between the old and new length values.
+Because of this length behavior, when JavaScript executes clothes.length = 0, all the items of the array clothes are deleted.
 clothes[0] is undefined, because clothes array was emptied.*/
 
 
