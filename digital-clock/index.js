2231ef7ac6f7cf6e0aae743f3fbb9558cd11b367
aditipuranik16 <61420553+aditipuranik16@users.noreply.github.com>
2021-01-30 14:07:16 +0530

    Add files via upload

---
+++ digital-clock/index.js
@@ -0,0 +1,44 @@
+function updateClock(){
+    var now = new Date();
+    var dname = now.getDay(),
+        month = now.getMonth(),
+        dnum = now.getDate(),
+        year = now.getFullYear(),
+        hour = now.getHours(),
+        min  = now.getMinutes(),
+        sec  = now.getSeconds(),
+        pe   = "AM";
+
+        if(hour == 0){
+            hour = 12;
+        }
+        if(hour > 12){
+            hour = hour - 12;
+            pe = "PM";
+        }
+
+        Number.prototype.pad = function(digits){
+            for(var n = this.toString(); n.length < digits; n = 0 + n);
+            return n;
+        }
+
+        var months = ["January","February","March","April","May",
+                      "June","July","August","September","October",
+                      "November","December"];
+        var week = ["Sunday","Monday","Tuesday","Wednesday",
+                    "Thursday","Friday","Saturday"];
+        var ids = ["dayname","month","daynum","year",
+                   "hour","minutes","seconds","period"];
+        var values = [week[dname],months[month],dnum.pad(2),year,hour.pad(2),
+                      min.pad(2),sec.pad(2),pe];
+         for(var i=0;i<ids.length;i++){
+             document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+
+         }
+
+}
+
+function initClock(){
+    updateClock();
+    setInterval("updateClock()",1);
+}
